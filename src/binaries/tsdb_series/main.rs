141f7cbb6b3706c7cbc1a853b3d07a43276f1e7e
wangyue1 <wangyue11.4@163.com>
2023-05-09 22:37:05 +0800

    feat: add binaries mod

---
+++ src/binaries/tsdb_series/main.rs
@@ -0,0 +1,36 @@
+use clap::Parser;
+use influxdb_common::iterator::AsyncIterator;
+use influxdb_storage::{operator, StorageOperator};
+use influxdb_tsdb::series::series_segment::SeriesSegment;
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Parser)]
+#[clap(about, version, author)]
+struct Config {
+    #[clap(long)]
+    pub path: String,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let config = Config::parse();
+    println!("config: {:?}", config);
+    if config.path.is_empty() {
+        println!("path MUST not be empty!");
+        return Ok(());
+    }
+
+    let op = operator()?;
+    let op = StorageOperator::new(op, config.path.as_str());
+    let segment = SeriesSegment::open(0, op).await?;
+
+    let mut itr = segment.series_iterator(0).await?;
+    let mut i = 0;
+    while let Some((entry, offset)) = itr.try_next().await? {
+        println!("{}>{:?} @{}", i, entry, offset);
+        i += 1;
+    }
+
+    Ok(())
+}
