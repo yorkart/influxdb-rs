5a839cb8cde7ca7647c6b541db677c7cbc6b3cea
wangyue1 <wangyue11.4@163.com>
2023-05-21 18:44:07 +0800

    feat: In a compromise mode, values cache is used to resolve  reference lifetime

--- src/tsdb/src/engine/tsm1/file_store/reader/block_iterator.rs
+++ src/tsdb/src/engine/tsm1/file_store/reader/block_iterator.rs
@@ -1,13 +1,17 @@
-use crate::engine::tsm1::block::decoder::FloatValueIterator;
-use common_base::iterator::{AsyncIterator, RefAsyncIterator, TryIterator};
-use influxdb_storage::opendal::Reader;
+use std::sync::Arc;
 
+use common_base::iterator::{AsyncIterator, RefAsyncIterator};
+use influxdb_storage::opendal::Reader;
+use tokio::sync::Mutex;
+
+use crate::engine::tsm1::block::decoder::FloatValueIterator;
 use crate::engine::tsm1::file_store::index::IndexEntries;
 use crate::engine::tsm1::file_store::reader::block_reader::TSMBlock;
 use crate::engine::tsm1::file_store::reader::index_reader::TSMIndex;
 use crate::engine::tsm1::file_store::reader::tsm_reader::ShareTSMReaderInner;
+use crate::engine::tsm1::value::values::BlockDecoder;
 use crate::engine::tsm1::value::values::Values;
-use crate::engine::tsm1::value::Value;
+use crate::engine::tsm1::value::{FloatValues, Value};
 
 #[async_trait]
 pub trait AsyncIteratorBuilder: Send + Sync {
@@ -25,7 +29,7 @@ where
     B: TSMBlock,
     I: TSMIndex,
 {
-    reader: Reader,
+    reader: Arc<Mutex<Reader>>,
     inner: ShareTSMReaderInner<I, B>,
 }
 
@@ -35,14 +39,18 @@ where
     I: TSMIndex,
 {
     pub(crate) fn new(reader: Reader, inner: ShareTSMReaderInner<I, B>) -> Self {
-        Self { reader, inner }
+        Self {
+            reader: Arc::new(Mutex::new(reader)),
+            inner,
+        }
     }
 
     async fn entries(&mut self, key: &[u8]) -> anyhow::Result<IndexEntries> {
+        let mut reader = self.reader.lock().await;
         let mut entries = IndexEntries::default();
         self.inner
             .index()
-            .entries(&mut self.reader, key, &mut entries)
+            .entries(&mut reader, key, &mut entries)
             .await?;
         Ok(entries)
     }
@@ -64,14 +72,14 @@ where
     {
         let entries = self.entries(key).await?;
         let itr: BlockIterator<B, I> =
-            BlockIterator::new(entries, &mut self.reader, self.inner.clone()).await?;
+            BlockIterator::new(entries, self.reader.clone(), self.inner.clone()).await?;
         Ok(Box::new(itr))
     }
 }
 
 /// BlockIterator allows iterating over each block in a TSM file in order.  It provides
 /// raw access to the block bytes without decoding them.
-pub struct BlockIterator<'a, B, I>
+pub struct BlockIterator<B, I>
 where
     B: TSMBlock,
     I: TSMIndex,
@@ -79,22 +87,22 @@ where
     entries: IndexEntries,
     i: usize,
 
-    reader: &'a mut Reader,
+    reader: Arc<Mutex<Reader>>,
     inner: ShareTSMReaderInner<I, B>,
 
     block: Vec<u8>,
 }
 
-impl<'a, B, I> BlockIterator<'a, B, I>
+impl<B, I> BlockIterator<B, I>
 where
     B: TSMBlock,
     I: TSMIndex,
 {
     pub(crate) async fn new(
         entries: IndexEntries,
-        reader: &'a mut Reader,
+        reader: Arc<Mutex<Reader>>,
         inner: ShareTSMReaderInner<I, B>,
-    ) -> anyhow::Result<BlockIterator<'a, B, I>> {
+    ) -> anyhow::Result<BlockIterator2<B, I>> {
         Ok(Self {
             entries,
             i: 0,
@@ -106,128 +114,58 @@ where
 }
 
 #[async_trait]
-impl<'a, B, I> AsyncIterator for BlockIterator<'a, B, I>
+impl<B, I> RefAsyncIterator for BlockIterator<B, I>
 where
     B: TSMBlock,
     I: TSMIndex,
 {
-    type Item = Values;
+    type Item<'b> = &'b [u8] where Self: 'b;
 
-    async fn try_next(&mut self) -> anyhow::Result<Option<Self::Item>> {
+    async fn try_next<'c>(&'c mut self) -> anyhow::Result<Option<Self::Item<'c>>> {
         if self.entries.entries.len() == 0 || self.i >= self.entries.entries.len() {
             return Ok(None);
         }
 
         let ie = self.entries.entries[self.i].clone();
         self.i += 1;
+
+        let mut reader = self.reader.lock().await;
         self.inner
             .block()
-            .read_block(&mut self.reader, ie, &mut self.block)
+            .read_block(&mut reader, ie, &mut self.block)
             .await?;
 
-        let values = Values::try_from((self.entries.typ, self.block.as_slice()))?;
-        Ok(Some(values))
+        Ok(Some(self.block.as_slice()))
     }
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/// BlockIterator allows iterating over each block in a TSM file in order.  It provides
-/// raw access to the block bytes without decoding them.
-pub struct BlockIterator2<'a, B, I>
+pub struct FloatFieldIterator<B, I>
 where
     B: TSMBlock,
     I: TSMIndex,
 {
-    entries: IndexEntries,
-    i: usize,
+    block_itr: BlockIterator2<B, I>,
 
-    reader: &'a mut Reader,
-    inner: ShareTSMReaderInner<I, B>,
-
-    block: Vec<u8>,
+    values: FloatValues,
+    step: usize,
 }
 
-impl<'a, B, I> BlockIterator2<'a, B, I>
+impl<B, I> FloatFieldIterator<B, I>
 where
     B: TSMBlock,
     I: TSMIndex,
 {
-    pub(crate) async fn new(
-        entries: IndexEntries,
-        reader: &'a mut Reader,
-        inner: ShareTSMReaderInner<I, B>,
-    ) -> anyhow::Result<BlockIterator2<'a, B, I>> {
-        Ok(Self {
-            entries,
-            i: 0,
-            reader,
-            inner,
-            block: vec![],
-        })
-    }
-}
-
-#[async_trait]
-impl<'a, B, I> AsyncIterator for BlockIterator2<'a, B, I>
-where
-    B: TSMBlock,
-    I: TSMIndex,
-{
-    type Item = Vec<u8>;
-
-    async fn try_next(&mut self) -> anyhow::Result<Option<Self::Item>> {
-        if self.entries.entries.len() == 0 || self.i >= self.entries.entries.len() {
-            return Ok(None);
-        }
-
-        let ie = self.entries.entries[self.i].clone();
-        self.i += 1;
-        self.inner
-            .block()
-            .read_block(&mut self.reader, ie, &mut self.block)
-            .await?;
-
-        Ok(Some(self.block.clone()))
-    }
-}
-
-pub struct FloatFieldIterator<'a, B, I>
-where
-    B: TSMBlock,
-    I: TSMIndex,
-{
-    block_buf: Vec<u8>,
-    block_itr: BlockIterator2<'a, B, I>,
-    field_itr: Option<FloatValueIterator<'a>>,
-}
-
-impl<'a, B, I> FloatFieldIterator<'a, B, I>
-where
-    B: TSMBlock,
-    I: TSMIndex,
-{
-    pub fn new(block_itr: BlockIterator2<'a, B, I>) -> Self {
+    pub fn new(block_itr: BlockIterator2<B, I>) -> Self {
         Self {
-            block_buf: vec![],
             block_itr,
-            field_itr: None,
+            values: vec![],
+            step: 0,
         }
     }
-
-    // async fn next_block(&'a mut self) -> anyhow::Result<Option<&[u8]>> {
-    //     if let Some(v) = self.block_itr.try_next().await? {
-    //         self.field_itr = Some(FloatValueIterator::new(v)?);
-    //     } else {
-    //         self.field_itr = None;
-    //     }
-    //
-    //     Ok(())
-    // }
 }
 
 #[async_trait]
-impl<'a, B, I> AsyncIterator for FloatFieldIterator<'a, B, I>
+impl<B, I> AsyncIterator for FloatFieldIterator<B, I>
 where
     B: TSMBlock,
     I: TSMIndex,
@@ -235,41 +173,21 @@ where
     type Item = Value<f64>;
 
     async fn try_next(&mut self) -> anyhow::Result<Option<Self::Item>> {
-        if self.field_itr.is_none() {
+        if self.values.len() == 0 || self.values.len() <= self.step {
             if let Some(v) = self.block_itr.try_next().await? {
-                self.block_buf = v;
-                self.field_itr = Some(FloatValueIterator::new(self.block_buf.as_slice())?);
+                (&mut self.values).decode(v)?;
+                if self.values.len() == 0 {
+                    return Ok(None);
+                }
+                self.step = 0;
             } else {
                 return Ok(None);
             }
         }
 
-        {
-            let field_itr = self.field_itr.as_mut().unwrap();
-            if let Some(v) = field_itr.try_next()? {
-                return Ok(Some(v));
-            }
-        }
+        let v = self.values[self.step].clone();
+        self.step += 1;
 
-        Ok(None)
-        // loop {
-        //     let field_itr = {
-        //         if let Some(v) = self.block_itr.try_next().await? {
-        //             Some(FloatValueIterator::new(v)?)
-        //         } else {
-        //             None
-        //         }
-        //     };
-        //
-        //     self.field_itr = field_itr;
-        //
-        //     if let Some(field_itr) = self.field_itr.as_mut() {
-        //         if let Some(v) = field_itr.try_next()? {
-        //             return Ok(Some(v));
-        //         }
-        //     } else {
-        //         return Ok(None);
-        //     }
-        // }
+        Ok(Some(v))
     }
 }
