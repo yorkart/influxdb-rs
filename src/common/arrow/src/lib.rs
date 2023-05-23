685ea18807ba558c42b6723c7e6ed3a20ee2131e
wangyue1 <wangyue11.4@163.com>
2023-05-24 07:50:16 +0800

    chore: add example chunk print

--- src/common/arrow/src/lib.rs
+++ src/common/arrow/src/lib.rs
@@ -1,5 +1,5 @@
 pub use arrow;
-pub use arrow_format;
+// pub use arrow_format;
 
 pub type ArrayRef = Box<dyn ::arrow::array::Array>;
 
--- src/tsdb/examples/tsm_chunk_reader.rs
+++ src/tsdb/examples/tsm_chunk_reader.rs
@@ -1,6 +1,7 @@
 use std::str::from_utf8_unchecked;
 
 use clap::Parser;
+use common_arrow::arrow::io::print::write;
 use common_base::point::series_field_key;
 use influxdb_storage::StorageOperator;
 use influxdb_tsdb::engine::tsm1::file_store::reader::tsm_reader::new_default_tsm_reader;
@@ -28,21 +29,26 @@ async fn main() -> anyhow::Result<()> {
     let tsm_reader = new_default_tsm_reader(op).await?;
     let mut b = tsm_reader.block_iterator_builder().await?;
 
-    let series = "cpu,host=server-09,region=uswest-00".as_bytes();
-    let fields = vec!["value".as_bytes(), "value_1".as_bytes()];
+    let series = "cpu,host=server-09,region=uswest-00";
+    let fields = vec!["value", "value_1"];
 
     for field in fields.as_slice() {
-        let key = series_field_key(series, field);
+        let key = series_field_key(series.as_bytes(), field.as_bytes());
         let typ = tsm_reader.block_type(key.as_slice()).await?;
 
         let key = unsafe { from_utf8_unchecked(key.as_slice()) };
         println!("{}: {}", key, typ);
     }
 
-    let mut v_itr = b.build(series, fields.as_slice()).await?;
-    while let Some(chunk) = v_itr.try_next().await? {
-        println!("{:?}", chunk);
+    let fs: Vec<&[u8]> = fields.iter().map(|f| f.as_bytes()).collect();
+    let mut chunk_itr = b.build(series.as_bytes(), fs.as_slice()).await?;
+    let mut chunks = Vec::new();
+    while let Some(chunk) = chunk_itr.try_next().await? {
+        println!("chunk len: {:?}", chunk.len());
+        chunks.push(chunk);
     }
+    let table_str = write(chunks.as_slice(), &["timestamp", &fields[0], &fields[1]]);
+    println!("{}", &table_str[..1000]);
 
     Ok(())
 }
